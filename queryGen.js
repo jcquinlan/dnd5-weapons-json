dfab54f5f2bd264b401f76a26f4e8fa694b5a104
Philipp Welsch <pwelsch.mmt-b2014@fh-salzburg.ac.at>
2015-04-29 16:11:29 +0200

    extended querygen.js

--- queryGen.js
+++ queryGen.js
@@ -1,12 +1,12 @@
 var fs = require('fs')
-var weapons = require('./weapons')
+//var weapons = require('./weapons')
 
 //insert into weapons values ("Barbed Dagger", "2d4", "Piercing", 10, 1, "Light, Finesse, Thrown (20/60)", True);
 
-var data = JSON.parse(fs.readFileSync('simpleMeleeWeapons.json', 'utf8'))
+var data = JSON.parse(fs.readFileSync('json/simpleMeleeWeapons.json', 'utf8'))
 var output = ""
 data.forEach(function(weapon) {
-	output += "insert into weapons values (\"" + weapon.Name + "\", " + "\"" + weapon.Cost + "\", " + "\"" + weapon.Damage ")\n";
+	output += "insert into weapons values (\"" + weapon.Name + "\", " + "\"" + weapon.Damage + "\", " + "\"" + weapon.Damagetype + "\"" + weapon.Cost + "\", " + "\"" + weapon.Weight + "\", " + "\"" + weapon.Cost + "\", " + ")\n";
 })
 
 console.log(output)
